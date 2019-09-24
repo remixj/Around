227c21fb631ce101f5d5f1ebbbe2e1d70ba6a395
remixj <jianghongxiang@bupt.edu.cn>
2019-09-24 14:27:39 -0700

    add addToBigTable function

--- main.go
+++ main.go
@@ -9,14 +9,28 @@ import (
 	"strconv"
 	"reflect"
 	"github.com/pborman/uuid"
+	"context"
+	"cloud.google.com/go/storage"
+	"cloud.google.com/go/bigtable"
+	"io"
+      	"github.com/auth0/go-jwt-middleware"
+      	"github.com/dgrijalva/jwt-go"
+      	"github.com/gorilla/mux"
+
 )
 
 const (
+	BUCKET_NAME = "post-images-253602"
 	DISTANCE = "200km"
 	INDEX = "around"
 	TYPE = "post"
-	ES_URL = "http://35.232.125.207:9200"
+	ES_URL = "http://34.70.202.237:9200"
+	PROJECT_ID = "around-253602"
+	BT_INSTANCE = "around-post"
 )
+
+// secrete value , we can change it to anything we want
+var mySigningKey = []byte("secret")
 
 type Location struct {
 	Lat float64 `json:"lat"`
@@ -27,6 +41,7 @@ type Post struct {
 	User 		string 	`json:"user"`
 	Message 	string 	`json:"message"`
 	Location 	Location `json:"location"`
+	Url 		string 	`json:"url"`
 }
 
 func main() {
@@ -63,26 +78,139 @@ func main() {
 	}
 
 	fmt.Println("start-service")
-	http.HandleFunc("/post",handlerPost)
-	http.HandleFunc("/search",handlerSearch)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	
+	r := mux.NewRouter()
+	
+	// jwtMiddleware is a verification method
+	// in old version code: "http://HandleFunc('/post',handlerPost)"
+	// when we accept post request and we will handle this request directly
+	// in new version code: "r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")"
+	// when we accept post request, we will first user the jwtMiddleware to test whether the token is the same or not and if the token is a perfect match, we will handle the request like old time. but if it goes south, it wil return err	
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter : func(token *jwt.Token)(interface{},error){
+			return mySigningKey , nil
+	},
+		SigningMethod:jwt.SigningMethodHS256,
+	})
+ 
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+      	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+      	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+      	http.Handle("/", r)
+      	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Received one post request.")
+	user := r.Context().Value("user")
+      	claims := user.(*jwt.Token).Claims
+      	username := claims.(jwt.MapClaims)["username"]
+
+	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	w.Header().Set("Access-Control-Allow-Headers","Content-Type,Authorization")
 	
-	decoder := json.NewDecoder(r.Body)
-	var p Post 
-	if err := decoder.Decode(&p); err != nil {
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from data
+	fmt.Println("Received one post request %s \n", r.FormValue("message"))
+	lat , _ := strconv.ParseFloat(r.FormValue("lat"),64)
+	lon , _ := strconv.ParseFloat(r.FormValue("lon"),64)
+
+	p := &Post{
+		User:    username.(string),
+		Message : r.FormValue("message"),
+		Location : Location{
+			Lat : lat,
+			Lon : lon,
+		},
+	}
+	
+      	id := uuid.New()
+
+	file , _ , err := r.FormFile("image")
+	if err != nil{
+		http.Error(w, "GCS is not setup",http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v \n", err)
+		panic(err)
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	_ , attrs, err := saveToGCS(ctx , file , BUCKET_NAME, id)
+	if err != nil{
+		http.Error(w , "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v \n", err)
 		panic(err)
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	p.Url = attrs.MediaLink
 	
-      	id := uuid.New()
       	// Save to ES.
-      	saveToES(&p, id)
+      	saveToES(p, id)
+	
+	// Save to big table
+	saveToBigTable(p,id)
+}
 
+func saveToBigTable(p *Post , id string) {
+	ctx := context.Background()
+	// you must update project name here
+	bt_client , err := bigtable.NewClient(ctx , PROJECT_ID , BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return 
+	}	
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+	
+	mut.Set("post", "user" , t , []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message)) 
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+ 
+	err = tbl.Apply(ctx, id, mut) 
+	if err != nil {
+ 		panic(err)
+		return
+	} 
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+}
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle , *storage.ObjectAttrs, error){
+	client , err := storage.NewClient(ctx)
+	if err != nil{
+		return nil , nil , err
+	}
+	defer client.Close()
+
+	bucket := client.Bucket(bucketName)
+	// Next check if the bucket exists
+	if _ , err = bucket.Attrs(ctx); err != nil {
+		return nil , nil , err
+	}
+
+	obj := bucket.Object(name)
+	w := obj.NewWriter(ctx)
+	if _ , err := io.Copy(w,r);err != nil{
+		return nil , nil , err
+	}
+	if err := w.Close(); err != nil{
+		return nil , nil , err
+	}
+
+	if err := obj.ACL().Set(ctx , storage.AllUsers , storage.RoleReader); err != nil{
+		return nil , nil , err
+	}
+
+	attrs , err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n" , attrs.MediaLink)
+	return obj , attrs , err
 }
 
 func saveToES(p *Post , id string){
